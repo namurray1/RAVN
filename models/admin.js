301e34b7ed03d8bfce3b4282452060a50d515b6d
Alex Murray <namurray1@gmail.com>
2018-10-03 14:44:23 -0400

    Add files under models directory

---
+++ models/admin.js
@@ -0,0 +1,71 @@
+module.exports = function (sequelize, DataTypes) {
+    var Admins = sequelize.define("Admins", {
+        full_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        organization_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true
+            }
+        },
+        address: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        phone: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        lat: {
+            type: DataTypes.DECIMAL(17, 14),
+            allowNull: true,
+            validate: {
+                min: -90,
+                max: 90
+            }
+        },
+        lng: {
+            type: DataTypes.DECIMAL(17, 14),
+            allowNull: true,
+            validate: {
+                min: -180,
+                max: 180
+            }
+        },
+        non_profit_id: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        }
+    }, {
+            // To create an association between Users and User
+            classMethods: {
+                associate: function (models) {
+                    //
+                    Admins.belongsTo(models.AllUsers);
+                }
+            }
+        })
+    return Admins;
+};
---
+++ models/animals.js
@@ -0,0 +1,131 @@
+module.exports = function (sequelize, DataTypes) {
+    var Animal = sequelize.define("Animal", {
+        pet_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        pet_type: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        gender: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        breed: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        summary: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        link_to_picture: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        weight: {
+            type: DataTypes.DECIMAL(3, 2),
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        temperament: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        special_needs: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        origin_phone: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                len: [10]
+            }
+        },
+        destination_phone: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                len: [10]
+            }
+        },
+        origin_address: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        destination_address: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        lat_origin: {
+            type: DataTypes.DECIMAL(17, 14),
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        lng_origin: {
+            type: DataTypes.DECIMAL(17, 14),
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        lat_destination: {
+            type: DataTypes.DECIMAL(17, 14),
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        lng_destination: {
+            type: DataTypes.DECIMAL(17, 14),
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        }
+    }
+        // // TODO
+        // // do we need to associate users and admins here?
+        // ,
+        // // To create an association between Users and Admins
+        // {
+        //     classMethods: {
+        //         associate: function(models) {
+        //             //
+        //             Animal.hasMany(models.Admins);
+        //         }
+        //     }
+        // }
+    )
+    return Animal;
+};
