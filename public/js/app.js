cbf780343ee445f4143464b62f83600075b4a589
Alex Murray <namurray1@gmail.com>
2018-10-05 00:13:15 -0400

    add package.json files

---
+++ public/js/app.js
@@ -0,0 +1,10 @@
+// const http = require('http');
+
+// const hostname = 
+// const port = 3000;
+
+// const server = http.createServer((req, res)) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-type', 'text/plain');
+//     res.end('Hello World!');
+// }
